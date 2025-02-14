fa0901bdafe01bb10d3806b214c23131cab28530
Scott McCrindle <iainmccrindle@gmail.com>
2025-02-14 14:25:18 -0500

    Completed first part of events lesson

--- lesson-6/js/events.js
+++ lesson-6/js/events.js
@@ -6,11 +6,14 @@ function random(number) {
     return Math.floor(Math.random()*(number + 1));
 }
 function bgChange() {
-    let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')'; // rgb(0-255,0-255,0-255)
     document.body.style.backgroundColor = rndCol;
 }
 // Event Listeners/Handlers
-btn.onclick = bgChange;
+// btn.onclick = bgChange;
+// btn.ondblclick = bgChange;
+// btn.onclick = bgChange;
+// window.onscroll = bgChange;
 
 
 /* STEP 1: Experiment with a variety of different events - comment out the above onclick listener and build an alternate one below - try the following - onfocus/onblur, ondblclick, onmouseover/onmouseout, window.onkeypress/onkeydown/onkeyup
@@ -21,14 +24,20 @@ btn.onclick = bgChange;
 // Comment out the above event listener/handler, and add the same event as an attribute directly to the button element
 
 /* STEP 3: Let's apply event handlers/listeners to a group of buttons - comment out the above BUTTON element, and create three new ones, A, B, and C */
-
-
+// const buttons = document.querySelectorAll("button");
+// console.log(buttons);
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].onclick = bgChange;
+// };
 /* STEP 4a: addEventListener() and removeEventListener()
 Let's rewrite the above code with these two methods - comment out the A, B, C buttons and restore the original button - then comment out the above code */
-
+// btn.addEventListener("dblclick", bgChange);
+// btn.removeEventListener("dblclick", bgChange);
 
 /* STEP 4b: We can also put the entire bgChange() function inside an anonymous function, if we'd like: */
-
+btn.addEventListener("click", function() {
+    bgChange();
+});
 
 
 /* STEP 4c: We can now remove the event listener … comment out the above code in STEP 4b, and then add another event listener, then remove it (silly, yes, but try it all the same) */
